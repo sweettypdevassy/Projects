04bd56bb3fc83ab0c5b1441ef6223a284a7f2dd0
Harisankar S <harisankars@ibm.com>
2024-12-04 15:33:57 +0530

    updated test.js

--- docker-cicd-demo/test.js
+++ docker-cicd-demo/test.js
@@ -1,10 +1,17 @@
 const request = require('supertest');
-const app = require('./app'); // Import the app
+const app = require('./app');
 
-describe('GET /', () => {
-  it('should return a 200 status code', async () => {
+let server; // Store the server instance
+
+beforeAll(() => {
+    server = app.listen(); // Start the server
+});
+
+afterAll((done) => {
+    server.close(done); // Close the server after all tests
+});
+
+test('GET /', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello, CI/CD Pipeline!');
-  });
 });
